7b47e318f77dec9b9bf729f34c084f715cc32292
wangchao <freetish@163.com>
2019-11-29 14:19:09 +0800

    update

--- manhole-plugin/src/main/groovy/com/freesith/manhole/plugin/ManholePlugin.groovy
+++ manhole-plugin/src/main/groovy/com/freesith/manhole/plugin/ManholePlugin.groovy
@@ -28,7 +28,7 @@ class ManholePlugin extends Transform implements Plugin<Project> {
             if (name != "implementation" && name != "compile") {
                 return
             }
-            configuration.dependencies.add(project.dependencies.create("com.github.freesith:mh:1cd449baa8"))
+            configuration.dependencies.add(project.dependencies.create("com.github.freesith:mh:e1fea74918"))
         }
         def android = project.extensions.getByType(AppExtension)
         android.registerTransform(this)
